d3dce3fb50b0d05783e5117c7c841fdee0a90308
charleschetty <cfunction@foxmail.com>
2022-06-26 00:09:17 +0800

    de

---
+++ fish/config.fish
@@ -0,0 +1,37 @@
+#if status is-interactive
+    # Commands to run in interactive sessions can go here
+#end
+
+# ~/.config/fish/config.fish
+
+
+export PATH="/home/charles/.local/bin:$PATH"
+export PATH="~/.npm-global/bin:$PATH"
+
+
+alias n=neofetch
+alias bty="sh ~/.config/fish/sh/bty.sh"
+alias provpn='export all_proxy=http://127.0.0.1:7890 '
+alias bqt="sh ~/.config/fish/sh/bqt.sh"
+alias stx=' ssh -i ~/.ssh/01.pem ubuntu@myip'
+alias sfx=' sftp -i ~/.ssh/01.pem ubuntu@myip'
+
+alias aliyun=' /home/charles/.local/bin/aliyundrive-webdav  --refresh-token="mytocken" --port=8080  --auth-user=myuser --auth-password=mypswd'
+alias bypm="sh ~/.config/fish/sh/bypm.sh"
+alias pw=poweroff
+alias boffice="sh ~/.config/fish/sh/boffice.sh"
+alias rb=reboot
+alias bkate="sh ~/.config/fish/sh/bkate.sh"
+alias bcode="sh ~/.config/fish/sh/bcode.sh"
+alias vpn="expressvpn connect"
+alias dvpn="expressvpn disconnect"
+alias ls="lsd"
+alias c="clear"
+alias cn="clear;n"
+alias du="dust"
+alias e="exa --icons"
+alias vi3="vim ~/.config/i3/config"
+alias top="gotop"
+n
+
+starship init fish | source
---
+++ fish/sh/bqt.sh
@@ -0,0 +1,1 @@
+for W in $(wmctrl -l |grep "Qt Creator" |awk '"'"'{print $1}'"'"'); do xprop -id $W -format _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $(printf 0x%x $((0xffffffff * 80 / 100))); done
