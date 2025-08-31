19bbca713b2f4730673f14361fdcf3bbb787bb02
charleschetty <cfunction@foxmail.com>
2025-08-31 18:59:33 +0800

    nvim fix bufferline theme

--- config/nvim/lua/user/cmp/lsp.lua
+++ config/nvim/lua/user/cmp/lsp.lua
@@ -1,13 +1,13 @@
 return function()
-	vim.lsp.enable("html")
+	--[[ vim.lsp.enable("html") ]]
 	vim.lsp.enable("clangd")
-	vim.lsp.enable("cmake")
+	--[[ vim.lsp.enable("cmake") ]]
 	vim.lsp.enable("jdtls")
 	vim.lsp.enable("lua_ls")
 	vim.lsp.enable("marksman")
 	vim.lsp.enable("pyright")
   vim.lsp.enable("r_language_server")
-  vim.lsp.enable("cssls")
+  --[[ vim.lsp.enable("cssls") ]]
   vim.lsp.enable("texlab")
   vim.lsp.enable("tinymist")
   vim.lsp.enable("ruff")
